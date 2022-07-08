ac45fba308279da1e92ef16a319c62f6617e4646
Allen / 임기원 <l4279625@gmail.com>
2022-07-09 00:41:18 +0900

    사이드바 리팩토링

--- .vuepress/getSidebar.ts
+++ .vuepress/getSidebar.ts
@@ -1,21 +1,20 @@
-import { readdirSync } from 'fs'
+import { readdirSync } from 'fs';
 
 const PATH = `${__dirname}/..`;
 
-function getFolderList(category: string) { 
-  return readdirSync(`${PATH}/${category}/`)
-    .filter((folderName) => folderName !== 'README.md');
+function getFolderList(category: string) {
+  return readdirSync(`${PATH}/${category}/`).filter((folderName) => folderName !== 'README.md');
 }
 
 function getChildren(category: string, folderName: string) {
-  return readdirSync(`${PATH}/${category}/${folderName}`)
-    .map(fileName => `/${category}/${folderName}/${fileName}`)
+  return readdirSync(`${PATH}/${category}/${folderName}`).map(
+    (fileName) => `/${category}/${folderName}/${fileName}`
+  );
 }
 
-export default (category = '') => getFolderList(category).map((folderName) => (
-  {
+export default (category = '') =>
+  getFolderList(category).map((folderName) => ({
     text: folderName,
     children: getChildren(category, folderName),
     collapsible: true,
-  }
-));
+  }));
